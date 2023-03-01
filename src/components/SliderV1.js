6434995f036a5b3e0aa4cf4e3d8acc5e1d1c2456
Luis Emmanuel <elcbolpl@gmail.com>
2023-03-01 00:44:54 -0600

    udpdate files
    commit

--- src/components/SliderV1.js
+++ src/components/SliderV1.js
@@ -1,59 +1,87 @@
 import React from 'react'
-import {Carousel} from "react-responsive-carousel"
+import {Carousel} from "react-responsive-carousel";
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 
-import img1 from '../assets/image1.png'
-import img2 from '../assets/image2.png' 
-import img3 from '../assets/image3.png'
+import img1 from '../assets/sibelly1.jpg'
+import img2 from '../assets/sibelly2.jpg' 
+import img3 from '../assets/sibelly3.jpg'
+import img4 from '../assets/sibelly4.jpg'
+import img5 from '../assets/sibelly5.jpg'
+import img6 from '../assets/sibelly6.jpg'
 import "./Slide.css"
-import {FaArrowCircleLeft,FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa'
+// import {FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa'
 
 
 
 const SliderV1 = () => {
-  return (
-    <Carousel showArrows="true" emulateTouch={true} className="main-slide "
-    axis="horizontal"
-  showStatus={false}
-  renderArrowPrev={(clickHandler, hasPrev) => {
-    return (
-      <div
-        className={`${
-          hasPrev ? "absolute" : "hidden"
-        } top-0 bottom-0 left-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
-        onClick={clickHandler}
-      >
-        <FaArrowAltCircleLeft/>
-      </div>
-    );
-  }}
-        
-    renderArrowNext={(clickHandler, hasNext) => {
-    return (
-      <div
-        className={`${
-          hasNext ? "absolute" : "hidden"
-        } top-0 bottom-0 right-0 flex justify-center items-center p-3 opacity-30 hover:opacity-100 cursor-pointer z-20`}
-        onClick={clickHandler}
-      >
-        <FaArrowAltCircleRight/>
-      </div>
-    );
-  }}
->
-        
-    <div>
-        <img src={img1} height="300px" width="200px" />
-    </div>
-    <div>
-        <img src={img2} height="300px" width="200px"/>
-    </div>
-    <div>
-        <img src={img3} height="300px" width="200px" />
-    </div>
-    
-</Carousel>
-  )
-}
+   
+  const arrowStyles = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+};
 
-export default SliderV1
+  return (
+    
+    
+    <Carousel
+      autoPlay
+      infiniteLoop
+      swipeable={true}
+      showArrows={true}
+      emulateTouch={true}
+      animateIn="fadeIn"
+      axis="horizontal"
+      centerSlidePercentage
+      dynamicHeight={true}
+      
+      renderIndicator={(onClickHandler, isSelected, index, label) => {
+        const defStyle = { marginLeft: 20, color: "white", cursor: "pointer" };
+        const style = isSelected
+          ? { ...defStyle, color: "red" }
+          : { ...defStyle };
+        return (
+          <span
+            style={style}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            value={index}
+            key={index}
+            role="button"
+            tabIndex={0}
+            aria-label={`${label} ${index + 1}`}
+          >
+            {"cust " + index}
+          </span>
+        );
+      }}
+    >
+      <div key="slide1" >
+        <img src={img1} />
+      </div>
+      <div key="slide1">
+        <img src={img2} />
+      </div>
+      <div key="slide1">
+        <img src={img3} />
+      </div>
+      <div key="slide1">
+        <img src={img4} />
+      </div>
+      <div key="slide1">
+        <img src={img5} />
+      </div>
+      <div key="slide1">
+        <img src={img6} />
+      </div>
+    </Carousel>
+    
+);
+    }
+
+
+export default SliderV1;
