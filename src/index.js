9e3962577b338972d0bb9964cecda9119aae1343
Luis Emmanuel <elcbolpl@gmail.com>
2023-04-15 23:07:17 -0600

    update
    comit commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
-const rootelement = document.getElementById('root');
-const root = createRoot(rootelement);
-  
 
-  root.render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
